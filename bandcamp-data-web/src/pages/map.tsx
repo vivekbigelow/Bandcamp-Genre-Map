719783e667c739dc6cfbb5b19cccbb9b0bd51f51
vivekbigelow <vivekbigelow1234@gmail.com>
2021-07-12 12:41:24 -0400

    DUE presentation

--- bandcamp-data-web/src/pages/map.tsx
+++ bandcamp-data-web/src/pages/map.tsx
@@ -1,101 +1,103 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import {
-    Link,
-    Button,
-    Flex,
-    Box,
-    Table,
-    Th,
-    Thead,
-    Tr,
-    Tbody,
-    Td,
-    Container
-  } from "@chakra-ui/react"
-import { Wrapper } from '../components/Wrapper';
-import { NavBar } from '../components/NavBar';
-import { useAlbumsQuery, useGenreByNameQuery } from '../generated/graphql';
-import { withUrqlClient } from 'next-urql';
-import { createUrqlClient } from '../utils/createUrqlClient';
-import GoogleMapReact from 'google-map-react';
-import { Marker } from '../components/Marker';
-import { GenreDropDown } from '../components/GenreDropDown';
+  Link,
+  Button,
+  Flex,
+  Box,
+  Table,
+  Th,
+  Thead,
+  Tr,
+  Tbody,
+  Td,
+  Container,
+} from "@chakra-ui/react";
+import { Wrapper } from "../components/Wrapper";
+import { NavBar } from "../components/NavBar";
+import { useAlbumsQuery, useGenreByNameQuery } from "../generated/graphql";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import GoogleMapReact from "google-map-react";
+import { Marker } from "../components/Marker";
+import { GenreDropDown } from "../components/GenreDropDown";
 
+interface mapProps {}
 
-interface mapProps {
+let key = "";
 
+if (process.env.NEXT_PUBLIC_GOOGLE_MAP_PUBLIC_KEY) {
+  key = process.env.NEXT_PUBLIC_GOOGLE_MAP_PUBLIC_KEY;
 }
 
 const Map: React.FC<mapProps> = ({}) => {
-    
-    const [value,setValue] = useState("");
-    const getValue = (e:React.ChangeEvent<HTMLSelectElement>) => {
-      setValue(e.target.value)
-    }
-    const [{data,fetching}] = useGenreByNameQuery({
-      variables:{
-        name:value
-      }
-    })
-    let body = null
-    let table = null
-    
+  const [value, setValue] = useState("");
+  const getValue = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setValue(e.target.value);
+  };
+  const [{ data, fetching }] = useGenreByNameQuery({
+    variables: {
+      name: value,
+    },
+  });
+  let body = null;
+  let table = null;
 
-    // data is loading
-    if(fetching){
-      body = (   <div style={{ height:'auto', width: '60%' }}>
-      <GoogleMapReact
-      bootstrapURLKeys= {{key: 'AIzaSyC0B05XnH-PvxOsus1k2halxIhWQZ1nLsk'}}
-      defaultCenter={{
-        lat: 40.00,
-        lng:-100.00
-      }}
-      defaultZoom={1}>
-        
-      </GoogleMapReact>
-    </div>);
-      //  no locations
-    } else if (!data?.genreByName){
-      body = (   <div style={{ height:'auto', width: '60%' }}>
-      <GoogleMapReact
-      bootstrapURLKeys= {{key: 'AIzaSyC0B05XnH-PvxOsus1k2halxIhWQZ1nLsk'}}
-      defaultCenter={{
-        lat: 40.00,
-        lng:-100.00
-      }}
-      defaultZoom={1}>
-        
-      </GoogleMapReact>
-    </div>);
-      } else {
-        
-      body = (
-        
-      
-        <div style={{ height:'auto', width: '60%' }}>
-          <GoogleMapReact
-          bootstrapURLKeys= {{key: 'key'}}
+  // data is loading
+  if (fetching) {
+    body = (
+      <div style={{ height: "auto", width: "60%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: key }}
           defaultCenter={{
-            lat: 40.00,
-            lng:-100.00
+            lat: 40.0,
+            lng: -100.0,
           }}
-          defaultZoom={1}>
-            {data.genreByName.albums.map(g_album => 
-              <Marker 
+          defaultZoom={1}
+        ></GoogleMapReact>
+      </div>
+    );
+    //  no locations
+  } else if (!data?.genreByName) {
+    body = (
+      <div style={{ height: "auto", width: "60%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: key }}
+          defaultCenter={{
+            lat: 40.0,
+            lng: -100.0,
+          }}
+          defaultZoom={1}
+        ></GoogleMapReact>
+      </div>
+    );
+  } else {
+    body = (
+      <div style={{ height: "auto", width: "60%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: key }}
+          defaultCenter={{
+            lat: 40.0,
+            lng: -100.0,
+          }}
+          defaultZoom={1}
+        >
+          {data.genreByName.albums.map((g_album) => (
+            <Marker
               album={{
-                artist:g_album.artist.name,
-                title:g_album.title.name,
-                url:g_album.url
-              }} 
-              key={g_album.title.name} 
-              lat={g_album.location.lat + (Math.random() * .008)} 
-              lng={g_album.location.lng + (Math.random() * .008)}/>
-            )}
-          </GoogleMapReact>
-        </div>
-      ) 
-      table = (
-        <Container  w='auto' overflow='scroll'>
+                artist: g_album.artist.name,
+                title: g_album.title.name,
+                url: g_album.url,
+              }}
+              key={g_album.title.name}
+              lat={g_album.location.lat + Math.random() * 0.008}
+              lng={g_album.location.lng + Math.random() * 0.008}
+            />
+          ))}
+        </GoogleMapReact>
+      </div>
+    );
+    table = (
+      <Container w="auto" overflow="scroll">
         <Table>
           <Thead>
             <Tr>
@@ -106,35 +108,40 @@ const Map: React.FC<mapProps> = ({}) => {
             </Tr>
           </Thead>
           <Tbody>
-            {data.genreByName.albums.map(g_album => 
+            {data.genreByName.albums.map((g_album) => (
               <Tr key={g_album.title.name}>
                 <Td>{g_album.title.name}</Td>
                 <Td>{g_album.artist.name}</Td>
                 <Td>{g_album.city.name}</Td>
                 <Td>{g_album.country.name}</Td>
-              </Tr>)}
+              </Tr>
+            ))}
           </Tbody>
         </Table>
-        </Container>
-      )
-      
-    }
-    return (
-        <>
-        
-        <NavBar />
-        <Flex>
+      </Container>
+    );
+  }
+  return (
+    <>
+      <NavBar />
+      <Flex>
         {body}
         <Flex placeContent="center" w="40%">
-        <div style={{height:'95vh', width:'90%', marginTop:'5px', overflow:'scroll' }}>
-          <GenreDropDown value={value} handleChange={(e) => getValue(e)} />
-          {table}
-        </div>
+          <div
+            style={{
+              height: "95vh",
+              width: "90%",
+              marginTop: "5px",
+              overflow: "scroll",
+            }}
+          >
+            <GenreDropDown value={value} handleChange={(e) => getValue(e)} />
+            {table}
+          </div>
         </Flex>
-        </Flex>
+      </Flex>
+    </>
+  );
+};
 
-        </>
-    );
-}
-
-export default withUrqlClient(createUrqlClient, { ssr: true })(Map)
+export default withUrqlClient(createUrqlClient, { ssr: true })(Map);
